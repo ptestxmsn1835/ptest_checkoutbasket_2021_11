a534d89379aa2646363243a8b44ff472ca622eb3
Unknown <petegeddis@gmail.com>
2021-11-20 16:52:17 +0000

    Code - Promotion skeleton

---
+++ CheckoutBasketLibrary/Promotion/IPromotion.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CheckoutBasketLibrary.Promotion
+{
+    public interface IPromotion
+    {
+        PromotionResult ApplyPromotion(List<BasketItem> items);
+    }
+}
---
+++ CheckoutBasketLibrary/Promotion/PromotionResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutBasketLibrary.Promotion
+{
+    public class PromotionResult
+    {
+        public int promotionTotalPrice = 0;
+        public List<BasketItem> promotionItems = new List<BasketItem>();
+        public List<BasketItem> nonPromotionItems = new List<BasketItem>();
+    }
+}
