9d252685f73185949a789f68d1fb04a6093fe104
Unknown <petegeddis@gmail.com>
2021-11-20 16:07:28 +0000

    Test - Invalid SKU items

---
+++ CheckoutBasketLibrary.Tests/BasketTestHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutBasketLibrary.Tests
+{
+    public static class BasketTestHelper
+    {
+        public static Basket TestCreateBasketItems(Basket basket, char sku, int quantity)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                basket.items.Add(new BasketItem() { SKU = sku });
+            }
+
+            return basket;
+        }
+    }
+}
--- CheckoutBasketLibrary.Tests/CheckoutBasketUnitTest.cs
+++ CheckoutBasketLibrary.Tests/CheckoutBasketUnitTest.cs
@@ -18,5 +18,20 @@ namespace CheckoutBasketLibrary.Tests
             Assert.True(response.isSuccessful);
             Assert.AreEqual(0, response.totalPrice);
         }
+
+        [Test]
+        public void CalculateBasketTotal_InvalidSKU()
+        {
+            var itemPriceData = new SKUPriceData();
+            var checkout = new Checkout(itemPriceData);
+            var basket = new Basket();
+            basket = BasketTestHelper.TestCreateBasketItems(basket, 'X', 1);
+            basket = BasketTestHelper.TestCreateBasketItems(basket, 'Y', 1);
+            basket = BasketTestHelper.TestCreateBasketItems(basket, 'Z', 1);
+
+            var response = checkout.CalculateBasketTotal(basket);
+            Assert.False(response.isSuccessful);
+            Assert.AreEqual("Checkout Exception - Invalid SKU(s) - X, Y, Z", response.errorMessage);
+        }
     }
 }
