7d996f7638a774d7427936992581c4af2f75b678
Unknown <petegeddis@gmail.com>
2021-11-20 17:06:31 +0000

    Test - Promotion 2xB for 45

--- CheckoutBasketLibrary.Tests/CheckoutBasketUnitTest.cs
+++ CheckoutBasketLibrary.Tests/CheckoutBasketUnitTest.cs
@@ -120,5 +120,38 @@ namespace CheckoutBasketLibrary.Tests
             Assert.True(response.isSuccessful);
             Assert.AreEqual(230, response.totalPrice);
         }
+
+
+        [Test]
+        public void CalculateBasketTotal_Promotion_2xB_for_45()
+        {
+            var itemPriceData = new SKUPriceData();
+            var promotions = new List<IPromotion>()
+            {
+            };
+            var checkout = new Checkout(itemPriceData, promotions);
+            var basket = new Basket();
+            basket = BasketTestHelper.TestCreateBasketItems(basket, 'B', 2);
+
+            var response = checkout.CalculateBasketTotal(basket);
+            Assert.True(response.isSuccessful);
+            Assert.AreEqual(45, response.totalPrice);
+        }
+
+        [Test]
+        public void CalculateBasketTotal_Promotion_2xB_for_45_WithRemainingBs()
+        {
+            var itemPriceData = new SKUPriceData();
+            var promotions = new List<IPromotion>()
+            {
+            };
+            var checkout = new Checkout(itemPriceData, promotions);
+            var basket = new Basket();
+            basket = BasketTestHelper.TestCreateBasketItems(basket, 'B', 3);
+
+            var response = checkout.CalculateBasketTotal(basket);
+            Assert.True(response.isSuccessful);
+            Assert.AreEqual(75, response.totalPrice);
+        }
     }
 }
