0a0e3493df504eef533b664cbc35c6b18d8625fe
Unknown <petegeddis@gmail.com>
2021-11-20 16:32:42 +0000

    Test - Single SKU basket total

--- CheckoutBasketLibrary.Tests/CheckoutBasketUnitTest.cs
+++ CheckoutBasketLibrary.Tests/CheckoutBasketUnitTest.cs
@@ -33,5 +33,57 @@ namespace CheckoutBasketLibrary.Tests
             Assert.False(response.isSuccessful);
             Assert.AreEqual("Checkout Exception - Invalid SKU(s) - X, Y, Z", response.errorMessage);
         }
+
+        [Test]
+        public void CalculateBasketTotal_NoPromotion_SKU_A()
+        {
+            var itemPriceData = new SKUPriceData();
+            var checkout = new Checkout(itemPriceData);
+            var basket = new Basket();
+            basket = BasketTestHelper.TestCreateBasketItems(basket, 'A', 1);
+
+            var response = checkout.CalculateBasketTotal(basket);
+            Assert.True(response.isSuccessful);
+            Assert.AreEqual(50, response.totalPrice);
+        }
+
+        [Test]
+        public void CalculateBasketTotal_NoPromotion_SKU_B()
+        {
+            var itemPriceData = new SKUPriceData();
+            var checkout = new Checkout(itemPriceData);
+            var basket = new Basket();
+            basket = BasketTestHelper.TestCreateBasketItems(basket, 'B', 1);
+
+            var response = checkout.CalculateBasketTotal(basket);
+            Assert.True(response.isSuccessful);
+            Assert.AreEqual(30, response.totalPrice);
+        }
+
+        [Test]
+        public void CalculateBasketTotal_NoPromotion_SKU_C()
+        {
+            var itemPriceData = new SKUPriceData();
+            var checkout = new Checkout(itemPriceData);
+            var basket = new Basket();
+            basket = BasketTestHelper.TestCreateBasketItems(basket, 'C', 1);
+
+            var response = checkout.CalculateBasketTotal(basket);
+            Assert.True(response.isSuccessful);
+            Assert.AreEqual(20, response.totalPrice);
+        }
+
+        [Test]
+        public void CalculateBasketTotal_NoPromotion_SKU_D()
+        {
+            var itemPriceData = new SKUPriceData();
+            var checkout = new Checkout(itemPriceData);
+            var basket = new Basket();
+            basket = BasketTestHelper.TestCreateBasketItems(basket, 'D', 1);
+
+            var response = checkout.CalculateBasketTotal(basket);
+            Assert.True(response.isSuccessful);
+            Assert.AreEqual(15, response.totalPrice);
+        }
     }
 }
