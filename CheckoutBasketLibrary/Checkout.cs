585ef7fb9efa9b5255a49e2b6886070713db423a
Unknown <petegeddis@gmail.com>
2021-11-20 16:29:10 +0000

    Code - Invalid SKU items
    Add validation and error handling

--- CheckoutBasketLibrary/Checkout.cs
+++ CheckoutBasketLibrary/Checkout.cs
@@ -18,8 +18,38 @@ namespace CheckoutBasketLibrary
         public CheckoutResponse CalculateBasketTotal(Basket basket)
         {
             var response = new CheckoutResponse();
-            response.isSuccessful = true;
+            try
+            {
+                if (ValidateBasket(basket))
+                {
+                    response.isSuccessful = true;
+                }
+            }
+            catch(KeyNotFoundException e)
+            {
+                response.errorMessage = e.Message;
+            }
+            catch(Exception e)
+            {
+                response.errorMessage = e.Message;
+            }
+
             return response;
+        }
+
+        private bool ValidateBasket(Basket basket)
+        {
+            if (basket.items.Count == 0)
+                return true;
+
+            var distinctInvalidSKUs = basket.items.Select(i => i.SKU).Where(sku => !skuPrices.ContainsKey(sku)).Distinct().OrderBy(sku => sku).ToList();
+
+            if(distinctInvalidSKUs.Count > 0)
+            {
+                throw new KeyNotFoundException($"Checkout Exception - Invalid SKU(s) - {string.Join(", ", distinctInvalidSKUs)}");
+            }
+
+            return true;
         }
     }
 
